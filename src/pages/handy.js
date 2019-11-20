7da89779c78f401f0e0fa19e1725eed6a69990c1
Melanie Tabroff <mtabroff@gmail.com>
2019-11-20 17:04:22 -0500

    added handy case study assets

--- src/pages/handy.js
+++ src/pages/handy.js
@@ -7,12 +7,7 @@ import "../components/systems.css"
 const HandyPage = () => (
     <Layout>
       <SEO title="Handy Case Study" />
-      <h1>This is the starter for the case study</h1>
-      <p>I am a curious soul, an honest worker, and a lover of making messy systems more understandable. I thrive in environments when I can collaborative with a team to solve meaningful problems.</p>
-      <div style={{ maxWidth: `200px`, marginBottom: `1.45rem` }}>
-        <img src={require('../assets/mel-avatar.png')} alt="Melanie" />
-  
-      </div>
+      <h1>Designing for Growth</h1>
       <Link to="/">Return to home</Link>
     </Layout>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,10 @@ import "../components/systems.css"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>I’m Melanie,<br></br>a Product Designer</h1>
+    <h1>I’m Melanie,<br></br>a Product Designer based in NYC </h1>
     <p>I am a curious soul, an honest worker, and a lover of making messy systems more understandable. I thrive in environments when I can collaborative with a team to solve meaningful problems.</p>
     <div style={{ maxWidth: `200px`, marginBottom: `1.45rem` }}>
-      <img src={require('../assets/mel-avatar.png')} alt="Melanie" />
+      <img src={require('../images/mel-avatar.png')} alt="Melanie" />
 
     </div>
     <Link to="/handy">Visit case study</Link>
